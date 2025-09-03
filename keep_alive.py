d7ea5be1324db2f9d7b2034716c07d7dba3728e9
idek-man <ejajahmed66@gmail.com>
2025-09-03 21:13:35 +0530

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,13 +1,10 @@
-# keep_alive.py
 import os
 from flask import Flask
 
 app = Flask(__name__)
 
-@app.get("/")
+@app.route("/")
 def root():
     return "Bot is running!", 200
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))
-    app.run(host="0.0.0.0", port=port)
+import bot
