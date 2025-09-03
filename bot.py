e905bf54f7e19f2cd7cf974792128eff18ba8c05
idek-man <ejajahmed66@gmail.com>
2025-09-03 21:03:40 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 import discord
 from discord.ext import commands
 from groq import Groq
-import keep_alive  # this imports the Flask app so gunicorn can serve it
+import keep_alive
 
 import os
 DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
