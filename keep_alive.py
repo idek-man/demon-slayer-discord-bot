5ca92e7ec456a0eeed28f0dcf5be5c33bfe0fef4
idek-man <ejajahmed66@gmail.com>
2025-09-03 23:28:44 +0530

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -8,3 +8,7 @@ def root():
     return "Bot is running!", 200
 
 import bot
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))  # 👈 must use $PORT
+    app.run(host="0.0.0.0", port=port)
